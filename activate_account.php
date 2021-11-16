4e9eed6ff4e70e29471f657a1fe4b57d092a50bf
adamkw66 <a.knox-warshaw@wustl.edu>
2021-11-15 19:28:07 -0600

    email verification and account activation files

---
+++ activate_account.php
@@ -0,0 +1,61 @@
+<?php
+
+require 'database.php';
+
+$email = mysql_escape_string($_GET['email']));
+$hash = mysql_escape_string($_GET['hash']);
+$confirmed = false;
+$message;
+
+//retrieve already registered usernames to make sure there are no duplicates
+$stmt1 = $mysqli->prepare("SELELCT hash FROM users WHERE email=? ");
+
+//print fail statement if query wasn't able to run
+if(!$stmt1){
+    printf("Query Prep Failed: %s\n", $mysqli->error);
+    exit;
+}
+
+$stmt1->bind_param('s', $email);
+$stmt1->execute();
+$stmt1->bind_result($hash_database);
+$stmt1->fetch();
+$stmt1->close();
+
+//checking if username is already taken or not
+if (strcmp($hash, $hash_database) == 0) {
+
+    $verified = true;
+
+    $stmt2 = $mysqli->prepare("INSERT into users (verified) VALUES (?)")
+    $stmt2->bind_param('s', $verified); //are booleans strings in mysql?
+    $stmt2->execute();	
+    $stmt2->close();
+
+    $message = '
+ 
+    Your account has been activated for the WashU Digital Marketplace!
+ 
+    You can log in here:
+    http://www.yourwebsite.com/home.php
+ 
+    '; // Our message above including the link
+}
+else {
+    $message = '
+ 
+    An error occured and your account was not activated for the WashU Digital Marketplace.
+
+    Please ensure that you use the exact link provided in the email verification email.
+ 
+    '; // Our message above including the link
+}
+
+ //SENDING CONFIRMATION EMAIL THAT ACCOUNT HAS BEEN CREATED
+ $to      = $email; // Send email to our user
+ $subject = 'WashU Digital Marketplace Account Activation'; // Give the email a subject 
+                     
+ $header = 'From:noreply@washudigitalmarketplace.com' . "\r\n"; // Set from headers
+
+ mail($to, $subject, $message, $header); // Send our email
+?>
---
+++ email_verification.php
@@ -0,0 +1,75 @@
+<?php
+
+require 'database.php';
+
+$username = mysql_escape_string($_POST['username']);
+$Password = mysql_escape_string($_POST['password']);
+$retype_password = mysql_escape_string($POST['retype_password']);
+$email = mysql_escape_string($_POST['email']);
+$duplicate_email = false;
+
+if ($username != NULl || $password == NULL || $retype_password == NULL || $email == NULL) { //is frontend going to ensure inputs aren't empty?
+    //ERROR MESSAGE NEED INPUT FOR ALL PARAMETERS
+}
+else if ($password != $retype_password) {
+    //ERROR MESSAGE PASSWORDS DON'T MATCH
+}
+else {
+
+    //retrieve already registered usernames to make sure there are no duplicates
+	$stmt1 = $mysqli->prepare("select email from users");
+
+	//print fail statement if query wasn't able to run
+	if(!$stmt1){
+		printf("Query Prep Failed: %s\n", $mysqli->error);
+		exit;
+	}
+
+	$stmt1->execute();
+	$stmt1->bind_result($registered_emails);
+
+	//checking if username is already taken or not
+	while($stmt1->fetch() && $duplicate_email == false){
+		if (strcmp($registered_emails, $email) == 0) {
+			$user_taken = true;
+		}
+	}
+	$stmt1->close();
+
+    if ($duplicate_email == false) {
+
+        //salt hashing password
+        $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+        $verified = false;
+
+        //creating a hash that will be checked upon user verification
+        $verification_hash = md5( rand(0,100000) );
+
+        //inserting account info into database
+        $stmt2 = $mysqli->prepare("INSERT into users (username, hashed_password, email, verified, verification_hash) VALUES (?, ?, ?, ?)")
+        $stmt2->bind_param('sssss', $username, $hashed_password, $email, $verified, $verification_hash); //are booleans strings in mysql?
+        $stmt2->execute();	
+        $stmt2->close();
+        
+        //SET UP FOR SENDING VERIFICATION EMAIL
+        $to      = $email; // Send email to our user
+        $subject = 'WashU Digital Marketplace Email Verification'; // Give the email a subject 
+        $message = '
+    
+        Thanks for signing up for WashU Digital Marketplace!
+        Your account has been created, you can login after you have activated your account by pressing the url below.
+    
+        Please click this link to activate your account:
+        http://www.yourwebsite.com/activate_account.php?email='.$email.'&verification_hash='.$verification_hash.'
+    
+        '; // Our message above including the link
+                        
+        $header = 'From:noreply@washudigitalmarketplace.com' . "\r\n"; // Set from headers
+
+        mail($to, $subject, $message, $header); // Send our email
+    }
+    else {
+        //looks like you already have an account mesage
+    }
+}
+?>
